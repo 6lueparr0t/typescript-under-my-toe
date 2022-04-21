919222b53ca6cc50c508ac9ee79d261cc895434c
6lueparr0t <6lueparr0t@gmail.com>
2022-04-21 16:46:02 +0900

    update code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # typescript-under-my-toe
 
 - 타입스크립트 & 리액트JS 프로젝트 관리
+
+## 보고 있는 강좌 목록
+
+- [https://www.inflearn.com/course/이펙티브-타입스크립트-스터디](https://www.inflearn.com/course/%EC%9D%B4%ED%8E%99%ED%8B%B0%EB%B8%8C-%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%8A%A4%ED%84%B0%EB%94%94)
+
+- [https://www.udemy.com/course/best-react/](https://www.udemy.com/course/best-react/)
---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "../../style/Expenses.css";
+
+const Expenses = (props) => {
+  const expensesList = props.items.map((expenseItem, index) => {
+    return (<ExpenseItem
+      key={index}
+      title={expenseItem.title}
+      amount={expenseItem.amount}
+      date={expenseItem.date}
+    />)
+  });
+  return (
+    <Card className="expenses">
+    {expensesList}
+    </Card>
+  );
+}
+export default Expenses;
