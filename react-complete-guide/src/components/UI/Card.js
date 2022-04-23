6d357b030a66922faf29ff6cce87e8620adcb73e
6lueparr0t <6lueparr0t@gmail.com>
2022-04-23 17:44:54 +0900

    management project

--- react-complete-guide/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import '../../style/UI/Card.css';
-
-const Card = (props) => {
-  const classes = 'card ' + props.className;
-
-  return (<div className={classes}>{props.children}</div>)
-}
-
-export default Card;
